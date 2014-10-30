6a4d74169502e9c2eb0ce2aae72a6f8bdf70d2e2
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2014-10-30 21:35:31 +0100

    ~ fixed test

--- com.wuetherich.osgi.ds.annotations.ds-annotation-builder/com.wuetherich.osgi.ds.annotations.test/src/com/wuetherich/osgi/ds/annotations/test/componentname/ComponentNameTest.java
+++ com.wuetherich.osgi.ds.annotations.ds-annotation-builder/com.wuetherich.osgi.ds.annotations.test/src/com/wuetherich/osgi/ds/annotations/test/componentname/ComponentNameTest.java
@@ -16,57 +16,52 @@ import com.wuetherich.osgi.ds.annotations.test.util.EclipseProjectUtils;
 
 public class ComponentNameTest extends AbstractDsAnnotationsTest {
 
-	@Test
-	public void test() throws Exception {
+  @Test
+  public void test() throws Exception {
 
-		//
-		EclipseProjectUtils.checkFileExists(getProject(),
-				Constants.COMPONENT_DESCRIPTION_FOLDER + "/de.test.Test.xml");
+    //
+    EclipseProjectUtils.checkFileExists(getProject(), Constants.COMPONENT_DESCRIPTION_FOLDER + "/de.test.Test.xml");
 
-		//
-		String expected_before = fromStream(getClass().getResourceAsStream(
-				this.getClass().getSimpleName() + "_Before.xml"));
-		//
-		String actual_before = fromStream(getProject().getFile(
-				Constants.COMPONENT_DESCRIPTION_FOLDER + "/de.test.Test.xml")
-				.getContents());
+    //
+    String expected_before = fromStream(getClass().getResourceAsStream(this.getClass().getSimpleName() + "_Before.xml"));
+    //
+    String actual_before = fromStream(getProject()
+        .getFile(Constants.COMPONENT_DESCRIPTION_FOLDER + "/de.test.Test.xml").getContents());
 
-		//
-		assertXMLEqual(expected_before, actual_before);
+    //
+    assertXMLEqual(expected_before, actual_before);
 
-		IFile javaSourcefile = getProject().getFile("src/de/test/Test.java");
-		InputStream sourceInput = getClass().getResourceAsStream(
-				this.getClass().getSimpleName() + ".input");
-		javaSourcefile.setContents(sourceInput, true, false, null);
+    IFile javaSourcefile = getProject().getFile("src/de/test/Test.java");
+    InputStream sourceInput = getClass().getResourceAsStream(this.getClass().getSimpleName() + ".input");
+    javaSourcefile.setContents(sourceInput, true, false, null);
 
-		//
-		final String expected_after = fromStream(getClass()
-				.getResourceAsStream(
-						this.getClass().getSimpleName() + "_After.xml"));
+    //
+    final String expected_after = fromStream(getClass().getResourceAsStream(
+        this.getClass().getSimpleName() + "_After.xml"));
 
-		//
-		Awaitility.await().until(new Callable<Boolean>() {
-			@Override
-			public Boolean call() throws Exception {
-				//
-				IFile file = getProject().getFile(
-						Constants.COMPONENT_DESCRIPTION_FOLDER
-								+ "/Hurz.xml");
+    //
+    Awaitility.await().until(new Callable<Boolean>() {
+      @Override
+      public Boolean call() throws Exception {
+        //
+        IFile file = getProject().getFile(Constants.COMPONENT_DESCRIPTION_FOLDER + "/Hurz.xml");
 
-				file.refreshLocal(IResource.DEPTH_INFINITE, null);
-				String actual_after = fromStream(file.getContents());
+        if (!file.exists()) {
+          return false;
+        }
 
-				//
-				return expected_after.equals(actual_after);
-			}
-		});
-	}
+        file.refreshLocal(IResource.DEPTH_INFINITE, null);
+        String actual_after = fromStream(file.getContents());
+        //
+        return expected_after.equals(actual_after);
+      }
+    });
+  }
 
-	@Override
-	protected SourceFile createSourceFile() {
-		return new SourceFile.Default(
-				"de/test/Test.java",
-				"package de.test; import org.osgi.service.component.annotations.Component; @Component public class Test {}");
-	}
+  @Override
+  protected SourceFile createSourceFile() {
+    return new SourceFile.Default("de/test/Test.java",
+        "package de.test; import org.osgi.service.component.annotations.Component; @Component public class Test {}");
+  }
 
 }
